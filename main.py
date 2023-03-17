b043e461ec99f3b91eba3646b0855d6a49c24249
Daniel Shmul <danielshmul@Daniels-MacBook-Pro.local>
2023-03-17 17:43:58 +0200

    Optimized number extry and fixed exponent and root operations

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from InquirerPy.validator import EmptyInputValidator
 from datetime import datetime
 import math
 
-
 def main():
     messages = {"Addition": ["first value", "second value"],
                 "Subtraction": ["first value", "second value"],
@@ -33,20 +32,8 @@ def main():
         
         # operations with two vairables
         if operation not in ["Date Delta", "Univariate Equation", "Quadratic Equation"]: 
-            val1 = inquirer.number(
-                message=f"Enter {messages[operation][0]}:",
-                float_allowed=True,
-                validate=EmptyInputValidator()
-            ).execute()
-
-            val2 = inquirer.number(
-                message=f"Enter {messages[operation][1]}:",
-                float_allowed=True,
-                validate=EmptyInputValidator()
-            ).execute()
-
-            val1 = float(val1)
-            val2 = float(val2)
+            val1 = float(inquire_number(messages[operation][0]))
+            val2 = float(inquire_number(messages[operation][1]))
 
             if operation == "Addition":
                 print(f"Result: {val1 + val2}")
@@ -56,10 +43,10 @@ def main():
                 print(f"Result: {val1 * val2}") 
             elif operation == "Division":
                 print(f"Result: {val1 / val2}")
-            elif operation == "Exponent": #TODO: fix error
-                print(f"Result: {val1 ^ val2}")
+            elif operation == "Exponent": 
+                print(f"Result: {val1 ** val2}")
             elif operation == "Root": #TODO: handle divide by 0
-                print(f"Result: {val1 ^ (1/val2)}")
+                print(f"Result: {val1 ** (1/val2)}")
         
         # alternate operations
         elif operation == "Date Delta": #TODO: add data validator
@@ -72,19 +59,41 @@ def main():
             ).execute()
 
             # convert date string to date object
-            date1 = datetime.strptime(date1_str, "%d/%m/%Y")
-            date2 = datetime.strptime(date2_str, "%d/%m/%Y")
+            try:
+                date1 = datetime.strptime(date1_str, "%d/%m/%Y")
+                date2 = datetime.strptime(date2_str, "%d/%m/%Y")
+            except:
+                print("Error: Invalid date provided.\n")
+                continue
             
-            # calculate different in days
+            # calculate difference in days
             delta = date2 - date1
             print(f"Result: {abs(delta.days)} days")
-            
+
         elif operation == "Univariate Equation":
             pass
-        elif operation == "Quadratic Equation":
-            pass
+        
+        elif operation == "Quadratic Equation": # TODO: fix logic
+            a = float(inquire_number(messages[operation][0]))
+            b = float(inquire_number(messages[operation][1]))
+            c = float(inquire_number(messages[operation][2]))
+
+            # calculate zeros
+            result1 = (-b + math.sqrt((b ** 2) - (4 * a * c))) / (2 * a)
+            result2 = (-b - math.sqrt((b ** 2) - (4 * a * c))) / (2 * a)
+
+            print(f"Results: {result1}, {result2}")
 
         print()
+
+def inquire_number(message):
+    result = inquirer.number(
+                message=f"Enter {message}:",
+                float_allowed=True,
+                replace_mode=True,
+                validate=EmptyInputValidator()
+            ).execute()
+    return result
 
 if __name__ == "__main__":
     main()
